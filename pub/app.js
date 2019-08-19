c34dbfa78695e83b8cff9c3ee0093dfe23aeefa5
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-08-19 15:16:24 +0100

    testimonials slider complete

--- pub/app.js
+++ pub/app.js
@@ -1,39 +1,27 @@
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
-  
-  // Close the Modal
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+$(document).ready(function() {
+
+  $("a").on('click',function(e) {
+    if (this.hash !== "") {
+      e.preventDefault()
+      var hash = this.hash
+    $('html,body').animate({
+      scrollTop: $(hash).offset().top
+    },800,function() {
+      window.location.hash = hash
+      });
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    });
+
+    window.onscroll = function() {
+      scrollFunction()
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+
+    const scrollFunction = () => {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollbutton").style.display = "block";
+      } else {
+        document.getElementById('scrollbutton').style.display = "none";
+      }
+    }
+  });
+
